7d29d2c8f38ef5aa8aa65aad99e5efa3352ae75b
rumoag <64068669+rumoag@users.noreply.github.com>
2021-02-24 15:07:12 +0100

    anadir

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const url_api= "https://api.airtable.com/v0/appU4eSR0p97dLVbc/Productos?maxRecords=3&view=Grid%20view";
 const url_api_borrar= "https://api.airtable.com/v0/appU4eSR0p97dLVbc/Productos?records[]="
 const Authorization = "Bearer keypFgW9ql6PGevJQ";
+const url_api_update = "https://api.airtable.com/v0/appU4eSR0p97dLVbc/Productos ";
 
 new Vue({
     el: '#app',
@@ -34,7 +35,75 @@ new Vue({
             })
         },
         actualizarProductoEnApi: function (id,nuevoTexto){
-            
-        }
+            fetch(url_api_update, {
+                headers: {
+                    'Authorization': Authorization,
+                    'Content-type': 'application/json'
+                },
+                //pacth un solo producto a la vez
+                method: 'PATCH',
+                body: JSON.stringify({
+                    "records": [
+                        {
+                            "id": id,
+                            "fields": {
+                                "Nombre": nuevoTexto,
+                            }
+                        },
+                    ]
+                })
+            })
+
+        },
+        actualizarAdqueridoEnApi: function (id,checked){
+            fetch(url_api_update, {
+                headers: {
+                    'Authorization': Authorization,
+                    'Content-type': 'application/json'
+                },
+                //pacth un solo producto a la vez
+                method: 'PATCH',
+                body: JSON.stringify({
+                    "records": [
+                        {
+                            "id": id,
+                            "fields": {
+                                "Adquerido": checked,
+                            }
+                        },
+                    ]
+                })
+            })
+            this.productos = this.productos.map((producto) => {
+                if (producto.id === id) {
+                    let miProducto = producto;
+                    miProducto.fields.Adquerido = checked;
+                    return miProducto;
+                } else {
+                    return producto
+                }
+            })
+
+        },
+        anyadirProductoApi: function (){
+            fetch(url_api_update, {
+                headers: {
+                    'Authorization': Authorization,
+                    'Content-type': 'application/json'
+                },
+                //pacth un solo producto a la vez
+                method: 'PATCH',
+                body: JSON.stringify({
+                    "records": [
+                        {
+                            "id": id,
+                            "fields": {
+                                "Nombre": nuevoTexto,
+                            }
+                        },
+                    ]
+                })
+            })
+        },
     },
 });
