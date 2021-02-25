94d847f47053cf4ba3e4fa48e569b557324e6c57
rumoag <64068669+rumoag@users.noreply.github.com>
2021-02-25 13:47:37 +0100

    fin

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-const url_api= "https://api.airtable.com/v0/appU4eSR0p97dLVbc/Productos?maxRecords=3&view=Grid%20view";
-const url_api_borrar= "https://api.airtable.com/v0/appU4eSR0p97dLVbc/Productos?records[]="
+
+const url_api= "https://api.airtable.com/v0/appLQkpWuUrYmsvG7/Table%201?view=Grid%20view";
+
+const url_api_create = "https://api.airtable.com/v0/appLQkpWuUrYmsvG7/Table%201";
+
+
 const Authorization = "Bearer keypFgW9ql6PGevJQ";
-const url_api_update = "https://api.airtable.com/v0/appU4eSR0p97dLVbc/Productos ";
-const url_api_create = "https://api.airtable.com/v0/appU4eSR0p97dLVbc/Productos";
+
 
 new Vue({
     el: '#app',
     data: {
-        productos: [],
-        textoActualizar: '',
-        nuevoProducto: '',
+        articulos: [],
+
     },
     mounted: function (){
         this.obtenerDatos();
@@ -22,93 +24,7 @@ new Vue({
                 },
             })
                 .then((response) => response.json())
-                .then((json) => this.productos = json.records)
-
-        },
-        borrarProducto: function (id){
-            fetch(url_api_borrar.concat(id), {
-                headers: {
-                    'Authorization' : Authorization,
-                },
-                method: 'DELETE'
-            });
-            this.productos = this.productos.filter(producto =>{
-                return producto.id !== id
-            })
-        },
-        actualizarProductoEnApi: function (id,nuevoTexto){
-            fetch(url_api_update, {
-                headers: {
-                    'Authorization': Authorization,
-                    'Content-type': 'application/json'
-                },
-                //pacth un solo producto a la vez
-                method: 'PATCH',
-                body: JSON.stringify({
-                    "records": [
-                        {
-                            "id": id,
-                            "fields": {
-                                "Nombre": nuevoTexto,
-                            }
-                        },
-                    ]
-                })
-            })
-
-        },
-        actualizarAdqueridoEnApi: function (id,checked){
-            fetch(url_api_update, {
-                headers: {
-                    'Authorization': Authorization,
-                    'Content-type': 'application/json'
-                },
-                //pacth un solo producto a la vez
-                method: 'PATCH',
-                body: JSON.stringify({
-                    "records": [
-                        {
-                            "id": id,
-                            "fields": {
-                                "Adquerido": checked,
-                            }
-                        },
-                    ]
-                })
-            })
-            this.productos = this.productos.map((producto) => {
-                if (producto.id === id) {
-                    let miProducto = producto;
-                    miProducto.fields.Adquerido = checked;
-                    return miProducto;
-                } else {
-                    return producto
-                }
-            })
-
-        },
-        anyadirProductoApi: function (nuevoTexto){
-            fetch(url_api_create, {
-                headers: {
-                    'Authorization': Authorization,
-                    'Content-type': 'application/json'
-                },
-                //pacth un solo producto a la vez
-                method: 'POST',
-                body: JSON.stringify({
-                    "records": [
-                        {
-                            "fields": {
-                                "Nombre": nuevoTexto,
-                                "Adquerido": false,
-
-                            }
-                        },
-                    ]
-                })
-            })
-                .then((json) => this.nuevoProducto = '')
-                .then((json) => this.obtenerDatos())
+                .then((json) => this.articulos = json.records)
 
         },
     },
